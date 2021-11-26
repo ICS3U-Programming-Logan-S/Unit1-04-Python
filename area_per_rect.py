e27bc7f943423c0e96ec0a76de40c45a398cf41d
Ubuntu <ubuntu@ip-172-31-91-42.ec2.internal>
2021-11-26 17:16:31 +0000

    created area-per_rect.py and basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Logan Sweeney
+# Created on: Nov 25, 2021
+# This program calculates and displays the area and perimeter of a rectangle.
+# rectangle with length 5cm and width 3cm.
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 6cm and width 7cm:")
+    print("")
+    print("Area = {} cm^2".format(6*7))
+    print("Perimeter = {} cm".format(2*(6+7)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Logan Sweeney
+# Created on: November 25, 2021
+# This program does basic math.
+
+def main():
+    print("99 + 6 = {}". format(99+6))
+    print("333 - 2946 = {}". format(333-2946))
+    print("8967 / 7 = {}". format(8967/7))
+    print("92836 - 2395 /  = {}". format(8-4/2))
+    print("2^3 = {}". format(2**3))
+    print("1 / 3 = {}". format(1/3))
+
+
+if __name__ == "__main__":
+    main()
